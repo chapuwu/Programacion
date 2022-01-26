38b549246ed7a3b7b4df257b57be8edfc1607235
chapi <lolochapa10@hotmail.com>
2022-01-26 10:13:58 -0300

    ejercicio16 javascript terminado

--- ejercicios javascript/exercise16.js
+++ ejercicios javascript/exercise16.js
@@ -1,2 +1,17 @@
-let name = document.querySelector('nombre')
+let boton = document.querySelector('.form')
 
+boton.addEventListener('submit', (event) => {
+    let nombre = document.querySelector('.nombre').value
+    let apellido = document.querySelector('.apellido').value
+    let edad = parseInt(document.querySelector('.edad').value)
+    if (nombre.length === 0) {
+        alert('en nombre no hay nada')
+    } else if (apellido.length === 0) {
+        alert('en apellido no hay nada')
+    } else if (isNaN(edad) || edad < 1) {
+        alert('edad invalida')
+    } else {
+        alert(`el nombre es: ${nombre}\nel apellido es: ${apellido}\nla edad es: ${edad}`)
+    }
+    event.preventDefault()
+})
