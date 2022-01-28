df598d052631e1f077a11ffe9d9edbf3fa61f55a
chapi <lolochapa10@hotmail.com>
2022-01-28 10:48:24 -0300

    probando clases
    se agrego armas para entenderlo mejor

--- programacion orientada a objetos (clase)/app.js
+++ programacion orientada a objetos (clase)/app.js
@@ -15,6 +15,23 @@ class Perro {
     }
 }
 
+class Arma {
+    constructor(modelo, balas, danio) {
+        this.modelo = modelo
+        this.balas = balas
+        this.danio = danio
+    }
+
+    disparar() {
+        if (this.balas === 0) {
+            console.log(`el/la ${this.modelo} no tiene balas`)
+        } else {
+            this.balas -= 1
+            console.log(`${this.modelo} hizo ${this.danio} y le quedan ${this.balas} balas`)
+        }
+    }
+}
+
 function main() {
     const perro1 = new Perro('Pepe', 'Salchicha')
     perro1.ladrar()
@@ -23,6 +40,21 @@ function main() {
     const perro2 = new Perro('JoseMaria', 'Policia')
     perro2.ladrar()
     perro2.decirRaza()
+
+    const ak47 = new Arma('AK-47', 30, 35)
+    ak47.disparar()
+    const m4a1 = new Arma('M4A1', 30, 30)
+    m4a1.disparar()
+    const usp = new Arma('USP', 12, 15)
+    usp.disparar()
+    const p90 = new Arma('P90', 50, 20)
+    p90.disparar()
+    const deagle = new Arma('Desert Eagle', 7, 37)
+    deagle.disparar()
+    const negev = new Arma('NEGEV', 100, 23)
+    negev.disparar()
+    const awp = new Arma('AWP', 10, 105)
+    awp.disparar()
 }
 
 main()
