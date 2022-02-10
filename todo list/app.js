bbce3b8a8aee211baec80a8cd2d9fde0efc0a8ab
chapi <lolochapa10@hotmail.com>
2022-02-10 11:05:17 -0300

    cambios minimos

--- todo list/app.js
+++ todo list/app.js
@@ -39,7 +39,7 @@ function addNewTodo(todo) {
     edit.classList.add('material-icons')
 
     // dandole funcionalidad a los botones
-    done.addEventListener('click', () => completeTodo(todo, li))
+    // done.addEventListener('click', () => completeTodo(todo, li))
     close.addEventListener('click', () => removeTodo(todo, li))
     edit.addEventListener('click', () => editTodo(todo, li))
 
@@ -60,7 +60,7 @@ function addNewTodo(todo) {
     ul.appendChild(li)
 }
 
-function completeTodo(todo, element) {}
+// function completeTodo(todo, element) {}
 
 function removeTodo(todo, element) {
     element.remove()
@@ -69,8 +69,8 @@ function removeTodo(todo, element) {
     const index = todos.indexOf(todo)
     if (index !== -1) {
         todos.splice(index, 1)
+        updateLocalStorage()
     }
-    updateLocalStorage()
 }
 
 function editTodo(todo, element) {
