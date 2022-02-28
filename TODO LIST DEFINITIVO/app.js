1252ef27f11d0ee461fb7982948ac739b071024e
ezeekiel <lolochapa10@hotmail.com>
2022-02-28 15:22:13 -0300

    css completo falta javascript

---
+++ TODO LIST DEFINITIVO/app.js
@@ -0,0 +1,7 @@
+const texto = document.querySelector('.texto')
+
+const botonEnviar = document.querySelector('.botonenviar')
+
+botonEnviar.addEventListener('click', () => {
+    const texto = ''
+})
