ec09cc1d576c3e138ac53abf0f3b6394dffa106c
chapi <lolochapa10@hotmail.com>
2022-03-26 22:38:03 -0300

    todo list typescript 100% hecho

--- practicando todo list TypeScript/app.js
+++ practicando todo list TypeScript/app.js
@@ -3,9 +3,6 @@ const texto = document.querySelector('.texto');
 const botonEnviar = document.querySelector('.botonenviar');
 const tareasIncompletas = document.querySelector('.tareas-incompletas > ul');
 const tareasCompletas = document.querySelector('.tareas-completas > ul');
-const botonEliminar = document.querySelector('.del');
-const botonFinalizar = document.querySelector('.terminado');
-const botonEditar = document.querySelector('.editar');
 const estado = {
     todosIncompletos: [],
     todosCompletos: [],
@@ -20,34 +17,76 @@ botonEnviar.addEventListener('click', () => {
     addTodo(texto.value);
     texto.value = '';
 });
-botonEliminar.addEventListener('click', () => {
-    const index = estado.todosIncompletos.indexOf(tareasIncompletas.innerHTML);
-    estado.todosIncompletos.splice(index, 1);
-    renderizar();
-});
 function addTodo(tarea) {
     estado.todosIncompletos.push(tarea);
     renderizar();
 }
 function addLiElement(tarea) {
     const li = document.createElement('li');
-    li.innerHTML = tarea;
-    const botones = document.createElement('button');
-    botones.classList.add('estilosBotones');
-    botones.classList.add('del');
-    botones.innerHTML = 'X';
-    li.appendChild(botones);
+    li.textContent = tarea;
     return li;
 }
 function renderizar() {
-    tareasIncompletas.innerHTML = '';
-    tareasCompletas.innerHTML = '';
+    tareasIncompletas.textContent = '';
+    tareasCompletas.textContent = '';
     estado.todosIncompletos.forEach((tarea) => {
         const li = addLiElement(tarea);
         tareasIncompletas.appendChild(li);
+        // BOTON BORRAR
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('estilosBotones');
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        deleteButton.addEventListener('click', () => {
+            const index = estado.todosIncompletos.indexOf(tarea);
+            estado.todosIncompletos.splice(index, 1);
+            renderizar();
+        });
+        // BOTON EDITAR
+        const editButton = document.createElement('button');
+        editButton.classList.add('estilosBotones');
+        editButton.textContent = '✏️';
+        li.appendChild(editButton);
+        editButton.addEventListener('click', () => {
+            const textoEditado = prompt('editar tarea:');
+            if (textoEditado !== null) {
+                const index = estado.todosIncompletos.indexOf(tarea);
+                estado.todosIncompletos[index] = textoEditado;
+                li.childNodes[0].textContent = textoEditado;
+            }
+        });
+        // BOTON TERMINADO, es pensarlo de otra forma
+        // totalmente distinta que esta bosta imperativa, react es declarativo
+        // programacion imperativa es decir paso a paso que hacer
+        // por ejemplo un ejempl ode la vida real, imperativo seria:
+        //     - poner harina
+        //       - poner agua
+        //         - poner la tanga de tu vieja
+        //         - amasar
+        //         - prende el horno
+        //          - ponelo adentro
+        //              - etc
+        // declarativo seria:
+        //     - HACE UN PAN
+        //       - COCINALO
+        // es mas facil, idiota
+        // javascript vanilla es imperativo, tenes uqe decirle cada cosa, como amasar, poner agua, etc
+        // en cambio en react es declarativo todo, decis que queres hacer, no como hacerlo
+        // react + typescript = <333 boe re max
+        const doneButton = document.createElement('button');
+        doneButton.classList.add('estilosBotones');
+        doneButton.innerHTML = '✔️';
+        li.appendChild(doneButton);
+        doneButton.addEventListener('click', () => {
+            estado.todosCompletos.push(tarea);
+            const index = estado.todosIncompletos.indexOf(tarea);
+            estado.todosIncompletos.splice(index, 1);
+            renderizar();
+        });
     });
     estado.todosCompletos.forEach((tarea) => {
         const li = addLiElement(tarea);
         tareasCompletas.appendChild(li);
     });
 }
+//# sourceMappingURL=app.js.map
--- practicando todo list TypeScript/app.ts
+++ practicando todo list TypeScript/app.ts
@@ -2,16 +2,13 @@ const texto: HTMLInputElement = document.querySelector('.texto')!
 const botonEnviar = document.querySelector('.botonenviar')
 const tareasIncompletas = document.querySelector('.tareas-incompletas > ul')
 const tareasCompletas = document.querySelector('.tareas-completas > ul')
-const botonEliminar = document.querySelector('.del')
-const botonFinalizar = document.querySelector('.terminado')
-const botonEditar = document.querySelector('.editar')
 
-interface ejemplo {
+interface Estado {
     todosIncompletos: string[]
     todosCompletos: string[]
 }
 
-const estado: ejemplo = {
+const estado: Estado = {
     todosIncompletos: [],
     todosCompletos: [],
 }
@@ -28,12 +25,6 @@ botonEnviar!.addEventListener('click', () => {
     texto.value = ''
 })
 
-botonEliminar!.addEventListener('click', () => {
-    const index = estado.todosIncompletos.indexOf(tareasIncompletas!.innerHTML)
-    estado.todosIncompletos.splice(index, 1)
-    renderizar()
-})
-
 function addTodo(tarea: string) {
     estado.todosIncompletos.push(tarea)
     renderizar()
@@ -41,22 +32,71 @@ function addTodo(tarea: string) {
 
 function addLiElement(tarea: string) {
     const li = document.createElement('li')
-    li.innerHTML = tarea
-    const botones = document.createElement('button')
-    botones.classList.add('estilosBotones')
-    botones.classList.add('del')
-    botones.innerHTML = 'X'
-    li.appendChild(botones)
+    li.textContent = tarea
     return li
 }
 
 function renderizar() {
-    tareasIncompletas!.innerHTML = ''
-    tareasCompletas!.innerHTML = ''
+    tareasIncompletas!.textContent = ''
+    tareasCompletas!.textContent = ''
 
     estado.todosIncompletos.forEach((tarea) => {
         const li = addLiElement(tarea)
         tareasIncompletas!.appendChild(li)
+
+        // BOTON BORRAR
+        const deleteButton = document.createElement('button')
+        deleteButton.classList.add('estilosBotones')
+        deleteButton.textContent = '❌'
+        li.appendChild(deleteButton)
+        deleteButton.addEventListener('click', () => {
+            const index = estado.todosIncompletos.indexOf(tarea)
+            estado.todosIncompletos.splice(index, 1)
+            renderizar()
+        })
+
+        // BOTON EDITAR
+        const editButton = document.createElement('button')
+        editButton.classList.add('estilosBotones')
+        editButton.textContent = '✏️'
+        li.appendChild(editButton)
+        editButton.addEventListener('click', () => {
+            const textoEditado = prompt('editar tarea:')
+            if (textoEditado !== null) {
+                const index = estado.todosIncompletos.indexOf(tarea)
+                estado.todosIncompletos[index] = textoEditado
+                li.childNodes[0].textContent = textoEditado
+            }
+        })
+
+        // BOTON TERMINADO, es pensarlo de otra forma
+        // totalmente distinta que esta bosta imperativa, react es declarativo
+        // programacion imperativa es decir paso a paso que hacer
+        // por ejemplo un ejempl ode la vida real, imperativo seria:
+        //     - poner harina
+        //       - poner agua
+        //         - poner la tanga de tu vieja
+        //         - amasar
+        //         - prende el horno
+        //          - ponelo adentro
+        //              - etc
+        // declarativo seria:
+        //     - HACE UN PAN
+        //       - COCINALO
+        // es mas facil, idiota
+        // javascript vanilla es imperativo, tenes uqe decirle cada cosa, como amasar, poner agua, etc
+        // en cambio en react es declarativo todo, decis que queres hacer, no como hacerlo
+        // react + typescript = <333 boe re max
+        const doneButton = document.createElement('button')
+        doneButton.classList.add('estilosBotones')
+        doneButton.innerHTML = '✔️'
+        li.appendChild(doneButton)
+        doneButton.addEventListener('click', () => {
+            estado.todosCompletos.push(tarea)
+            const index = estado.todosIncompletos.indexOf(tarea)
+            estado.todosIncompletos.splice(index, 1)
+            renderizar()
+        })
     })
 
     estado.todosCompletos.forEach((tarea) => {
