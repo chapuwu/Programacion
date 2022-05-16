2c8cc4247cd8bce786466ba74207d238edd1f191
ezeekiel <58433204+chapuwu@users.noreply.github.com>
2022-05-16 16:14:20 -0300

    pizzeriafamilia

---
+++ pizzeria/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className='bg-gray-500 h-screen w-screen'>
+            <h1>probando</h1>
+        </div>
+    )
+}
+
+export default App
