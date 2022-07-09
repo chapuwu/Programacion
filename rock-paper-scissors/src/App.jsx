2882e847adf1a420b3a4a9e5c6d2d882cfcefb37
Mis <misdocumeno@hotmail.com>
2022-07-09 00:06:20 -0300

    arreglando tus cagadas

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -2,7 +2,7 @@ import Game from './components/Game'
 
 function App() {
     return (
-        <div className='h-screen w-screen bg-slate-700'>
+        <div className='h-screen w-screen bg-slate-700 flex flex-col items-center'>
             <Game />
         </div>
     )
--- rock-paper-scissors/src/components/Points.jsx
+++ rock-paper-scissors/src/components/Points.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Points({ text }) {
     return (
-        <div className='bg-green-700 h-16 mt-8 rounded-lg w-1/4 flex justify-center items-center'>
+        <div className='bg-green-700 px-28 py-4 m-3 rounded-lg flex justify-center items-center'>
             <p className='text-slate-800 text-xl font-bold'>{text}</p>
         </div>
     )
